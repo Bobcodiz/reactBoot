d27ec3d8dee4ec821f00322faa7012a3d31cc0f8
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-03-09 07:20:30 +0300

    ...

--- my-app/src/pages/HomePage.jsx
+++ my-app/src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import { Container } from "react-bootstrap";
+import Landing from "./Landing";
 
 export default function HomePage(){
     return(
         <>
-            <Container></Container>
+            <Container>
+                <Landing/>
+            </Container>
         </>
     )
 }
