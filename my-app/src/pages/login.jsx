5e2d5f328ff73780039cbeef208e09bd9e23fd1e
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-03-10 18:51:01 +0300

    ...

--- my-app/src/pages/login.jsx
+++ my-app/src/pages/login.jsx
@@ -1,8 +1,14 @@
+import { Container, Form } from "react-bootstrap";
 
 
 export default function login(){
     return(
         <>
+            <Container>
+                <Form as={Form} className="loginform">
+                    
+                </Form>
+            </Container>
         </>
     )
 }
