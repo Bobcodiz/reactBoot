ead883344df094ef2269c3f18acfc760a44f83d1
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-03-10 22:42:32 +0300

    more better way of implementing counter with useState hook

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,14 @@
 import {Container} from "react-bootstrap";
 import Home from "./components/authorization/Home";
 import UseStateHook from "./components/UseStateHook";
+import UseStateTwo from "./components/UseStateTwo";
 
 function App() {
   return (
     <div className="App">
         {/*<Registration/>*/}
         <Container >
-          <UseStateHook/>     
+          <UseStateTwo/>  
         </Container>
 
     </div>
---
+++ my-app/src/components/UseStateTwo.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import { Button ,Stack} from 'react-bootstrap'
+
+function UseStateTwo() {
+    const initial = 0
+    const [count,setCount] = useState(initial)
+
+    const Increase = () =>{
+        for(let i = 0;i < 5; i++){
+            setCount(prev => prev + 1)
+        }
+    }
+  return (
+    <div>
+        count:{count}
+        <Stack direction='horizontal'>
+            <Button onClick={()=>setCount(prev=>prev+1)}>Increment</Button>
+            <Button onClick={()=>setCount(prev=>prev - 1)}> Decrement</Button>
+            <Button onClick={Increase}>Increment Five</Button>
+        </Stack>
+    </div>
+  )
+}
+
+export default UseStateTwo
